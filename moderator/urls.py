ce9ea3859c334a86f0d3801f46488083b5d72fe7
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-08-15 00:51:34 +0530

    added dashboard view

--- moderator/urls.py
+++ moderator/urls.py
@@ -3,4 +3,6 @@ from moderator import views
 
 urlpatterns = [
     path('ignition/', views.ignition),
+    path('', views.dashboard),
+    path('dashboard/', views.dashboard),
 ]
--- moderator/views.py
+++ moderator/views.py
@@ -1,7 +1,16 @@
 import os
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, JsonResponse
 from django.conf import settings
 
 def ignition(request):
     return HttpResponse('Run Successfull!', status='200')
+
+@login_required
+def dashboard(request):
+    if request.user.is_staff:
+        context = {}
+        return render(request, 'moderator/dashboard.html', context)
+    else:
+        return HttpResponse('You are not allowed to view this content!')
